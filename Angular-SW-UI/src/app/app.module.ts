40f5a0d5682057b2fb78d4f851e925a7098419eb
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2019-02-08 23:36:28 -0200

    Components and pages declared and HTTP response interceptor provided

--- Angular-SW-UI/src/app/app.module.ts
+++ Angular-SW-UI/src/app/app.module.ts
@@ -2,32 +2,81 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-//Components
-import { AppComponent } from './app.component';
-
 //Routers
 import { AppRoutingModule } from './app-routing.module';
 
-//Services
-import { FilmService } from './services/film.service';
-
 //Interceptors
 import { UrlInterceptor } from './interceptors/url.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+
+//Components
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { FilmItemComponent } from './components/film-item/film-item.component';
+import { StarshipItemComponent } from './components/starship-item/starship-item.component';
+import { VehicleItemComponent } from './components/vehicle-item/vehicle-item.component';
+import { PlanetItemComponent } from './components/planet-item/planet-item.component';
+import { CharacterItemComponent } from './components/character-item/character-item.component';
+import { SpecieItemComponent } from './components/specie-item/specie-item.component';
+
+//Pages
+import { ErrorComponent } from './pages/error/error.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { FilmComponent } from './pages/film/film.component';
+import { StarshipsComponent } from './pages/starships/starships.component';
+import { StarshipComponent } from './pages/starship/starship.component';
+import { SpecieComponent } from './pages/specie/specie.component';
+import { SpeciesComponent } from './pages/species/species.component';
+import { PlanetComponent } from './pages/planet/planet.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { VehicleComponent } from './pages/vehicle/vehicle.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        //Components
+        MenuComponent,
+        PaginationComponent,
+        StarshipItemComponent,
+        VehicleItemComponent,
+        PlanetItemComponent,
+        CharacterItemComponent,
+        SpecieItemComponent,
+        FilmItemComponent,
+
+        //Pages
+        AppComponent,
+        ErrorComponent,
+        FilmsComponent,
+        FilmComponent,
+        StarshipsComponent,
+        StarshipComponent,
+        SpeciesComponent,
+        SpecieComponent,
+        PlanetsComponent,
+        PlanetComponent,
+        CharactersComponent,
+        CharacterComponent,
+        VehiclesComponent,
+        VehicleComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule,
+        AppRoutingModule
     ],
     providers: [
-        FilmService,
         {
             provide: HTTP_INTERCEPTORS,
             useClass: UrlInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ResponseInterceptor,
             multi: true
         }
     ],
