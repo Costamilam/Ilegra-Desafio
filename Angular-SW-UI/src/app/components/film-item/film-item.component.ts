112bf6bc0ddfca6552b0e953d07703b7982cc33e
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2019-02-08 23:21:24 -0200

    Film component added

---
+++ Angular-SW-UI/src/app/components/film-item/film-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-film-item',
+    templateUrl: './film-item.component.html',
+    styleUrls: ['./film-item.component.css']
+})
+export class FilmItemComponent {
+
+    @Input() film: object;
+
+    @Input() reverse: boolean = false;
+
+    constructor() { }
+
+}
